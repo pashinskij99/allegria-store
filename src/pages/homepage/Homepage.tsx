ebbfb30d932114b4e0f7fba637064b928233e1cd
pashinskij99 <pashinskij99@icloud.com>
2023-02-28 18:21:21 +0200

    first commit

---
+++ src/pages/homepage/Homepage.tsx
@@ -0,0 +1,9 @@
+import styles from './Homepage.module.scss';
+
+interface HomepageProps { }
+
+export const Homepage = ({ }: HomepageProps) => (
+  <div className={styles.homepage}>
+    Homepage
+  </div>
+);
